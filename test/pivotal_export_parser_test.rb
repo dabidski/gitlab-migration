26f9085753d084bcf402cb6b68307e03db6df621
djbelmonte <belmontedonnjerico@gmail.com>
2021-03-05 06:59:07 +0000

    Add test for accepted_at

--- test/pivotal_export_parser_test.rb
+++ test/pivotal_export_parser_test.rb
@@ -12,29 +12,40 @@ class PivotalExportParserTest < Minitest::Test
           :labels => "",
           :type => "feature",
           :current_state => "accepted",
-          :owned_by => []
+          :owned_by => nil,
+          :accepted_at => DateTime.parse("Nov 2, 2018")
         },
         "161714920" => {
           :title => "Unable to add documents to Ocorian Room",
           :labels => "ocorian, qa2",
           :type => "bug",
           :current_state => "accepted",
-          :owned_by => ["Amy Bath", "David Sumulong"]
+          :owned_by => ["Amy Bath", "David Sumulong"],
+          :accepted_at => DateTime.parse("Nov 6, 2018")
         },
         "161580247" => {
           :title => "Expose inherited metadata fields for documents & folders [part 3 - with inheritance]",
           :labels => "api, seeunity",
           :type => "feature",
           :current_state => "accepted",
-          :owned_by => ["David Sumulong"]
+          :owned_by => ["David Sumulong"],
+          :accepted_at => DateTime.parse("Nov 16, 2018")
+        },
+        "170098998" => {
+          :title => "^^ Waiting on go ahead from JLee ^^",
+          :labels => "",
+          :type => "release",
+          :current_state => "unstarted",
+          :owned_by => nil,
+          :accepted_at => nil
         }
       }
 
       expected = expected_data[story[:id]] or raise "Cannot find story with ID: #{story[:id]}"
       expected.keys.each do |key|
-        assert_equal expected[key], story[key]
+        expected[key].nil? ? assert_nil(story[key]) : assert_equal(expected[key], story[key])
       end
     end
-    assert_equal 3, count
+    assert_equal 4, count
   end
 end
